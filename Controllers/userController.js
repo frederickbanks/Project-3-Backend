fad96690b2dff97ab63437510cdd368beabfed39
Melissa Price <melissaashprice@gmail.com>
2019-05-22 15:50:42 -0400

    added login & signup functionality

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -8,7 +8,66 @@ const config = require('../config/config')
 
 const User = require('../Models/userModel')
 
+//SIGNUP POST REQUEST (create token for when user signs up)
+router.post('/signup', (req,res) => {
+    // if there is a username and passord set values
+    if(req.body.username && req.body.password){
+        let newUser = {
+            username: req.body.username,
+            password: req.body.password
+        }
+        User.findOne({username: req.body.username})
+        .then((user) => {
+            //if user doesn't exist create a new user
+            if(!user) {
+                User.create(newUser)
+                .then(user => {
+                    if(user) {
+                        var payload = {
+                            id: newUser.id
+                        }
+                        var token = jwt.encode(payload, config.jwtSecret)
+                        res.json({
+                            token: token
+                        })
+                    } else{
+                        res.sendStatus(401)
+                    }
+                })
+            } else {
+                res.sendStatus(401)
+            }
+        })
+    } else {
+        res.sendStatus(401)
+    }
+})
 
+
+//USER LOGIN(CREATE TOKEN FOR USER LOGIN)
+router.post('/login', (req, res) => {
+    if (req.body.username && req.body.password) {
+      User.findOne({ username: req.body.username }).then(user => {
+        if (user) {
+          if (user.password === req.body.password) {
+            var payload = {
+              id: user.id
+            }
+            var token = jwt.encode(payload, config.jwtSecret)
+            res.json({
+              token: token
+            })
+          } else {
+            res.sendStatus(401)
+          }
+        } else {
+          res.sendStatus(401)
+        }
+      })
+    } else {
+      res.sendStatus(401)
+    }
+  })
 
 
 
--- Models/userModel.js
+++ Models/userModel.js
@@ -8,10 +8,4 @@ const UserSchema = new Schema({
 
 
 
-<<<<<<< HEAD
-module.export = mongoose.model('User', UserSchema);
-=======
-module.export  = mongoose.model('User', UserSchema);
-
-
->>>>>>> 253afc6164e3a9c5ce2ce7c7654840e243503c8f
+module.exports = mongoose.model('User', UserSchema);
