7b195eae43fad2c3abe87ced45b054ff086cdc4d
Melissa Price <melissaashprice@gmail.com>
2019-05-22 00:22:34 -0400

    basic JWT setup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const parser = require('body-parser')
 const cors = require('cors')
+const helmet = require('helmet');
+const morgan = require('morgan')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 
 const app = express()
 
@@ -11,7 +15,26 @@ const app = express()
 app.use(parser.urlencoded({extended: true}));
 app.use(parser.json())
 
+//USE HELMET
+app.use(helmet())
+// Log http Requests with morgan
+app.use(morgan('combined'))
+
 //Use of Controllers go here
+
+
+
+const checkToken = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: ``
+    }),
+     audience: 'k5CXT5TwLiD3T1xmoRjLQy8yG6aVxFEL',
+     issuer: `https://dropz.auth0.com/`,
+     algorithms: ['RS256']
+})
 
 
 //local port
